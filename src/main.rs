d3e44532f483c2ba81104da4ca1198b5a6ec3755
Foulgaz3 <tkdayden@gmail.com>
2024-08-22 22:42:55 -0400

    began work on backprop. Confirmed loss calculation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use rand::prelude::*;
 use ndarray::{Array, Array1, ArrayView1};
+use std::env;
 
 fn forward(x: ArrayView1<f32>, theta: ArrayView1<f32>) -> Array1<f32> {
     let mut result = Array1::from_elem(x.raw_dim(), theta[0]);
@@ -12,8 +13,24 @@ fn forward(x: ArrayView1<f32>, theta: ArrayView1<f32>) -> Array1<f32> {
     return result;
 }
 
-
-type param = (f64, f64, f64);
+/// Performs forward and back propagation
+/// returns (loss, gradient)
+/// 
+/// TODO: blend forward and backward computation
+fn forwardback(x: ArrayView1<f32>, y: ArrayView1<f32>, theta: ArrayView1<f32>) -> (f32, Array1<f32>) {
+    let yhat = forward(x, theta);
+    let dLdyhat = yhat - y;
+    let pN = 1./(x.len() as f32);
+    let mean = |val: Array1<f32>| {val.sum() * pN};
+    let loss = mean(dLdyhat.clone()*dLdyhat.view())*0.5;
+    let mut gradient = Array1::zeros(theta.raw_dim());
+    // let mut tmp_x = Array1::<f32>::ones(theta.raw_dim());
+    // for i in 0..gradient.len() {
+    //     gradient[i] = mean(dLdyhat.clone()*tmp_x.clone());
+    //     tmp_x = tmp_x * x
+    // }
+    return (loss, gradient)
+}
 
 #[cfg(test)]
 mod tests {
@@ -29,9 +46,17 @@ mod tests {
 }
 
 fn main() {
+    env::set_var("RUST_BACKTRACE", "1");
     let param = Array1::from_vec(vec![3.4, 2.9, 4.5]);
-    let arr = Array1::from_vec(vec![1., 2., 3., 4., 5., 6., 7., 8.]);
-    let modified = forward(arr.view(), param.view());
-    println!("Array: {}", arr);
-    println!("Modified: {}", modified);
+    let param2 = Array1::from_vec(vec![3., 3., 5.]);
+
+    let X = Array1::from_vec(vec![1., 2., 3., 4., 5., 6., 7., 8.]);
+    let Y = forward(X.view(), param.view());
+    let Yhat = forward(X.view(), param2.view());
+    let (loss, gradient) = forwardback(X.view(), Y.view(), param2.view());
+
+    println!("Y: {}", Y);
+    println!("Yhat: {}", Yhat);
+    
+    println!("Loss: {}", loss);
 }
